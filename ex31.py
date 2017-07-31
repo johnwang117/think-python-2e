0c1da14c1998046d99a2159b6a38ea89bcaee227
johnwang117 <seraph.enzo@gmail.com>
2017-07-31 16:16:48 +0800

    update file name

--- ex31.py
+++
@@ -1,4 +0,0 @@
-def right_justify(s):
-	lenght_of_s = len(s)
-	lenght_of_space = 70 - lenght_of_s
-	print(' ' * lenght_of_space + s)
--- ex32-2.py
+++
@@ -1,8 +0,0 @@
-def do_twice(f, value):
-	f(value)
-	f(value)
-
-def print_spam(spam):
-	print(spam)
-
-do_twice(print_spam, 'maps')
